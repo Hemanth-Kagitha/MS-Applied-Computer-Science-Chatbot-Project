6432cc4ee0a39e03a742841194ebf24bbd9f4406
Hemanth-Kagitha <123528403+Hemanth-Kagitha@users.noreply.github.com>
2024-04-18 13:20:30 -0500

    chat history

--- ui_app.py
+++ ui_app.py
@@ -33,25 +33,23 @@ if "messages" not in st.session_state:
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    if message["role"] == "user":
+        st.text("User: " + message["content"])
+    elif message["role"] == "assistant":
+        st.text("Assistant: " + message["content"])
 
 # React to user input
-if prompt := st.text_input("Enter you query"):
-    # Display user message in chat message container
-    #st.echo()
-    #st.markdown(f"user: {prompt}")
+if prompt := st.text_input("Enter your query"):
+    # Display user input
+    st.text("User: " + prompt)
 
-    
-    st.chat_message("user").markdown(prompt)
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
     response = genai_engine(prompt)
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        st.markdown(response)
-    # Add assistant response to chat history
+    # Display bot response
+    st.text("Assistant: " + response)
+    # Add bot response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
 
 display_feedback_form()
